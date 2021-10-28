7b9cd0bf2474cc8cf33ca397c684e97b14653420
Kevigan <kevigan88@yahoo.com>
2021-10-28 15:34:22 +0200

    unity fit all screens

--- CloudMobile/Assets/Scripts/BoarderScript.cs
+++ CloudMobile/Assets/Scripts/BoarderScript.cs
@@ -9,13 +9,23 @@ public class BoarderScript : MonoBehaviour
     [SerializeField] private bool leftBoarder;
     [SerializeField] private bool rightBoarder;
 
+    Vector3 cameraPos = Vector3.zero;
+    private void Start()
+    {
+        float cameraWidth = Screen.width;
+        if (rightBoarder) cameraPos = Camera.main.ScreenToWorldPoint(new Vector3(cameraWidth + 100, transform.position.y));
+        if (leftBoarder) cameraPos = Camera.main.ScreenToWorldPoint(new Vector3(-100, transform.position.y));
+
+        transform.position = cameraPos;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.GetComponent<PlayerCharacter2D>() is PlayerCharacter2D player)
+        if (collision.GetComponent<PlayerCharacter2D>() is PlayerCharacter2D player)
         {
             player.transform.position = new Vector3(oppositeBoarder.transform.position.x + distance, player.transform.position.y);
         }
-        if(collision.GetComponent<Cloud>() is Cloud cloud && leftBoarder)
+        if (collision.GetComponent<Cloud>() is Cloud cloud && leftBoarder)
         {
             //cloud.transform.position = new Vector3(oppositeBoarder.transform.position.x + distance, cloud.transform.position.y);
             cloud.TurnRight();
